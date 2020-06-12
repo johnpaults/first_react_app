286b34396da60f1e2e14dc6a3e31706a2f6cc556
John Paul <tsjohnpaul92@gmail.com>
2020-06-12 18:23:53 +0530

    two way binding added

--- src/App.js
+++ src/App.js
@@ -19,11 +19,20 @@ class App extends Component {
     );
   }
 
+  updateName = (event) => {
+    this.setState({
+      persons: [
+        { name: event.target.value, age: `28` },
+        { name: "Jyoti", age: `25` }]
+    }
+    );
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Welcome to my first react app</h1>
-        <PersonComponent name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <PersonComponent updatedName = {this.updateName} name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <PersonComponent click = {this.changePersonState.bind(this, 'and she is a software Engineer')} name={this.state.persons[1].name} age={this.state.persons[1].age} />
         <button onClick={() => this.changePersonState('and he is a Senior Software Engineer')}>Add Profession</button>
       </div>
--- src/Person/PersonComponent.js
+++ src/Person/PersonComponent.js
@@ -4,6 +4,7 @@ const PersonComponent = (props) => {
     return (
         <div>
             <p onClick = {props.click}>My name is {props.name} and age is {props.age}</p>
+            <input type = 'text' onChange = {props.updatedName} value = {props.name} />
         </div>
     );
 }
