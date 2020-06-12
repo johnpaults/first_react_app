31c13576b992f22b495c90dfd8c648a233b1ff96
John Paul <tsjohnpaul92@gmail.com>
2020-06-12 18:35:34 +0530

    added css files

--- src/Person/PersonComponent.js
+++ src/Person/PersonComponent.js
@@ -1,8 +1,9 @@
 import React from "react";
+import './PersonComponent.css';
 
 const PersonComponent = (props) => {
     return (
-        <div>
+        <div className='Person'>
             <p onClick = {props.click}>My name is {props.name} and age is {props.age}</p>
             <input type = 'text' onChange = {props.updatedName} value = {props.name} />
         </div>
