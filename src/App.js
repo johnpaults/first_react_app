ad5049cebcbccfe12333d14d0a44c785d119643e
John Paul <tsjohnpaul92@gmail.com>
2020-06-25 12:41:39 +0530

    Rendering content conditionally

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ class App extends Component {
     persons: [
       { name: "John", age: "28" },
       { name: "Jyoti", age: "25" }
-    ]
+    ],
+    showPersons: false
   }
 
   changePersonState = (profession) => {
@@ -28,13 +29,23 @@ class App extends Component {
     );
   }
 
+  toggleNames = () => {
+    let doesName = this.state.showPersons;
+    this.setState({showPersons: !doesName});
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Welcome to my first react app</h1>
-        <PersonComponent updatedName = {this.updateName} name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <PersonComponent click = {this.changePersonState.bind(this, 'and she is a software Engineer')} name={this.state.persons[1].name} age={this.state.persons[1].age} />
-        <button onClick={() => this.changePersonState('and he is a Senior Software Engineer')}>Add Profession</button>
+        <button onClick= {this.toggleNames}> Toggle Names </button>
+        { this.state.showPersons ?
+          <div>
+            <PersonComponent updatedName={this.updateName} name={this.state.persons[0].name} age={this.state.persons[0].age} />
+            <PersonComponent click={this.changePersonState.bind(this, 'and she is a software Engineer')} name={this.state.persons[1].name} age={this.state.persons[1].age} />
+            <button onClick={() => this.changePersonState('and he is a Senior Software Engineer')}>Add Profession</button>
+          </div> : null
+        }
       </div>
     );
   }
